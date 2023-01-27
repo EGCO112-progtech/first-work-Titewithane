8f4bb7b2bf0b82f87d42542194760effbd3b699e
Titewithane <123083698+Titewithane@users.noreply.github.com>
2023-01-27 07:50:19 +0000

    spacebarsong

--- .ccls-cache/@home@runner@first-work-Titewithane-1/main.c
+++ .ccls-cache/@home@runner@first-work-Titewithane-1/main.c
@@ -7,14 +7,13 @@
 
 #include <stdio.h>
 
-int main(int argc, char * argv[]) {
-    
-  printf("argc is %d\n",argc);
-  int i,N;
-  for(i=1;i<argc;i++){
-      printf("arg %d: %s\n",i,argv[i]);
-        
+int main(int argc, char *argv[]) {
+
+  printf("argc is %d\n", argc);
+  int i;
+  for (i = 1; i < argc; i++) {
+    printf("arg %d: %s\n", i, argv[i]);
   }
-    
-    return 0;
+
+  return 0;
 }
